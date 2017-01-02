0625def45e8e8bba0817f8f7995abcba53e1ef30
Darmo <Darmo@PC>
2017-01-02 14:07:52 +0100

    feat : changememt de l'aspect des tableaux

--- Smart Tennis Sensor Analyser/src/net/darmo_creations/gui/InfoPanel.java
+++ Smart Tennis Sensor Analyser/src/net/darmo_creations/gui/InfoPanel.java
@@ -1,9 +1,14 @@
 package net.darmo_creations.gui;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableModel;
 
 import net.darmo_creations.controller.DataContainer;
@@ -18,6 +23,7 @@ public class InfoPanel extends JPanel {
 
   public InfoPanel(String unitName1, String unitName2, String unit, boolean displayProportion) {
     setLayout(new GridLayout(1, 1));
+    setBorder(new EmptyBorder(5, 5, 5, 5));
 
     String[][] data = new String[displayProportion ? 5 : 4][];
     int i = 0;
@@ -30,6 +36,11 @@ public class InfoPanel extends JPanel {
     data[i++] = new String[]{unitName2 + " min", ""};
 
     this.table = new JTable(data, new String[]{"", ""});
+    this.table.setEnabled(false);
+    this.table.setShowGrid(false);
+    this.table.setIntercellSpacing(new Dimension());
+    this.table.getColumnModel().getColumn(0).setCellRenderer(new CellRenderer());
+    this.table.getColumnModel().getColumn(1).setCellRenderer(new CellRenderer());
     this.unit = unit;
     this.displayProportion = displayProportion;
 
@@ -42,9 +53,23 @@ public class InfoPanel extends JPanel {
 
     t.setValueAt(data != null ? "" + data.getNbUnit1() : "-", i++, 1);
     if (this.displayProportion)
-      t.setValueAt((data != null ? String.format("%.2f", data.getProportion()) : "-") + " %", i++, 1);
-    t.setValueAt((data != null ? "" + data.getMax() : "-") + " " + this.unit, i++, 1);
-    t.setValueAt((data != null ? "" + data.getAverage() : "-") + " " + this.unit, i++, 1);
-    t.setValueAt((data != null ? "" + data.getMin() : "-") + " " + this.unit, i++, 1);
+      t.setValueAt((data != null ? String.format("%.2f %%", data.getProportion()) : "-"), i++, 1);
+    t.setValueAt((data != null ? "" + data.getMax() + " " + this.unit : "-"), i++, 1);
+    t.setValueAt((data != null ? "" + data.getAverage() + " " + this.unit : "-"), i++, 1);
+    t.setValueAt((data != null ? "" + data.getMin() + " " + this.unit : "-"), i++, 1);
+  }
+
+  private class CellRenderer extends DefaultTableCellRenderer {
+    private static final long serialVersionUID = -7121306538259351109L;
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+      Component cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+      setBorder(new EmptyBorder(5, 5, 5, 5));
+      cellComponent.setBackground(column == 0 ? Color.LIGHT_GRAY : new Color(240, 240, 240));
+
+      return cellComponent;
+    }
   }
 }
